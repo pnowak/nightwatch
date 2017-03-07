23903cc5754abdfa525fc85577f60fc8ee7366a7
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-03-07 20:03:59 +0100

    night improve

--- helpers/night.js
+++ helpers/night.js
@@ -1,45 +1,39 @@
 'use strict';
 
-module.exports = function nigth(url, data) {
-  	const getresponse = new Nightmare({
-      openDevTools: {
-        mode: 'detach'
-      },
-      show: true
+module.exports = function nigth(url, data) {    
+    const getresponse = new Nightmare({
+        openDevTools: {
+            mode: 'detach'
+        },
+        show: true
     })
-
-    .goto(url)
+    .goto('https://app.getresponse.com/login.html')
     .wait()
-    .type('#login', '')
-    .type('#passwdInp2', '')
+    .type('#login', '...')
+    .type('#passwdInp2', '...')
     .click('#loginFrm [type=submit]')
     .wait(1000)
     .goto('http://app.getresponse.com/message_basic_settings.html?type=broadcast')
     .wait(1000)
     .type('#subject', 'Nigth test')
     .click('.submit [type=submit]')
-    .wait(1000)
-    .goto('https://app.getresponse.com/message_choose_template.html')
-    .wait(1000)
-    .click('form[action*="message_choose_template.html"] .templates .template .image')
-    .wait(1000)
+    .wait('.templates')
+    .click('.template .use-template span')
     .goto('https://app.getresponse.com/message_create_message.html')
-    .wait(1000)
-    .click('#form-createmessage .editorBottomBar [href="#htmleditor"]')
-    .wait(1000)
+    .click('#form-createmessage .editorBottomBar a[href="#htmleditor"]')
     .click('#codeEditorAlert [data-close=ok]')
-    .wait(5000)
+    .wait(1000)
     .visible('#editor-content')
     .click('#editor-content')
     .evaluate(function() {
-        return document.getElementById("editor-content").value = data;
+        document.getElementById('editor-content').value = data;
     })
     .end()
     .click('[href="#htmleditor"]')
     .wait(1000)
     .click('#codecode')
     .evaluate(function() {
-        return document.getElementById("editor-content").value;
+        document.getElementById('editor-content').value;
     })
     .end()
 
